ef8b916aeac91da99eac28925c205b0a253bed01
Sdpalmaq <sdpalmaq@utn.edu.ec>
2025-01-16 08:14:56 -0500

    add url host

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://tesis-backend-xro2.onrender.com/api',
   withCredentials: true,
 });
 
